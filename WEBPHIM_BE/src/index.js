189bb9341442c902b2a3e9fc01dd50b7fd7c6122
chinh <chinh28062004@gmail.com>
2026-04-26 17:36:54 +0700

    fixx cor

--- WEBPHIM_BE/src/index.js
+++ WEBPHIM_BE/src/index.js
@@ -7,21 +7,17 @@ const app = express();
 // 1. Middleware
 app.use(cors());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
+
 app.use(express.json());
 
 // 2. Routes
 const allRoutes = require('./routes');
 app.use('/api', allRoutes);
-
-// 3. Error handler (LUÔN TRƯỚC listen cũng được, nhưng phải sau routes)
-app.use((err, req, res, next) => {
-    console.error("ERROR:", err);
-
-    res.status(err.statusCode || 500).json({
-        success: false,
-        message: err.message || 'Internal Server Error'
-    });
-});
 
 // 4. Start server
 const PORT = process.env.PORT || 5000;
